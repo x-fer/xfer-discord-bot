52904a8a343de0911e08a00af477d80ea7ec05f6
Ivan Vlahov <ivan.vlahov@gmail.com>
2021-11-01 01:15:27 +0100

    error handling u cf naredbi

--- commands/cf.cjs
+++ commands/cf.cjs
@@ -14,31 +14,40 @@ module.exports = async function(msg, args){
         var handle = args.shift();
 
         let url = `https://codeforces.com/api/user.info?handles=${handle}`;
-        let response = await fetch(url);
-        let json = await response.json();
 
-        if(json.status != 'OK'){
+        try{
+            let response = await fetch(url);
+            let json = await response.json();
+
+            if(json.status != 'OK'){
+                var message = new Discord.MessageEmbed()
+                    .setColor('#ff5f6d')
+                    .setDescription('Something went wrong while fetching user info!');
+
+                msg.channel.send({embeds: [message]});
+            }else{
+                
+                var message = new Discord.MessageEmbed()
+                    .setTitle(String(json.result[0].handle))
+                    .setImage(json.result[0].avatar)
+                    .setColor('#ff5f6d')
+                    .setURL(`https://codeforces.com/profile/${json.result[0].handle}`)
+                    .addFields(
+                        { name: 'Trenutni rating: ', value: String(json.result[0].rating), inline: true},
+                        { name: 'Trenutni title: ', value: String(json.result[0].rank), inline: true},
+                        { name: 'Max. rating: ', value: String(json.result[0].maxRating), inline: true},
+                        { name: 'Max. title: ', value: String(json.result[0].maxRank), inline: true}
+                    );
+
+                msg.channel.send({embeds: [message]});
+            }
+        } catch (error) {
             var message = new Discord.MessageEmbed()
                 .setColor('#ff5f6d')
                 .setDescription('Something went wrong while fetching user info!');
 
             msg.channel.send({embeds: [message]});
-        }else{
-            
-            var message = new Discord.MessageEmbed()
-                .setTitle(String(json.result[0].handle))
-                .setImage(json.result[0].avatar)
-                .setColor('#ff5f6d')
-                .setURL(`https://codeforces.com/profile/${json.result[0].handle}`)
-                .addFields(
-                    { name: 'Trenutni rating: ', value: String(json.result[0].rating), inline: true},
-                    { name: 'Trenutni title: ', value: String(json.result[0].rank), inline: true},
-                    { name: 'Max. rating: ', value: String(json.result[0].maxRating), inline: true},
-                    { name: 'Max. title: ', value: String(json.result[0].maxRank), inline: true}
-                );
-
-            msg.channel.send({embeds: [message]});
-        }
+        };
     }
 
     
